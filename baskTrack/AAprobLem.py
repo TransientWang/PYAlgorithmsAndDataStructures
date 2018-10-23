4384117527128530f02cd96f31b38f10fa54062a
wfy815775 <815775754@qq.com>
2018-10-24 00:18:40 +0800

    AA制分配问题

---
+++ baskTrack/AAprobLem.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+
+if __name__ == '__main__':
+    list = [30.0, 40.0, 60.0, 70.0,10.0,196.0]
+    mList = []
+    r = [[0.0 for i in range(len(list))] for i in range(len(list))]  # 辅助数组 r[i][j] 代表i应该给j的钱数
+    mid = 0.0
+    for i in range(0, len(list)):
+        mid += list[i]
+    mid /= len(list);
+    print(mid)
+    for i in range(0, len(list)):
+        mList.append(mid - list[i])
+    print(mList)
+    for i in range(0, len(list)):
+        for j in range(0, len(list)):
+            if i == j:
+                continue
+            else:
+                if mList[i] < 0 and mList[j] > 0:
+                    mList[i] += mList[j]
+                    r[j][i] = mList[j]
+                    mList[j] -= mList[j]
+
+    for i in range(0, len(list)):
+        for j in range(0, len(list)):
+            if r[i][j] <> 0:
+                print(i, " gave ",r[i][j]," to ", j)
+    print(mList)
