5c07b015c04a188ef2fdd832e86befb8374d66b1
wfy815775 <815775754@qq.com>
2018-10-23 21:59:22 +0800

    动态规 游艇问题

--- baskTrack/DynamicSelector.py
+++ baskTrack/DynamicSelector.py
@@ -7,33 +7,32 @@ if __name__ == '__main__':
          [0, 0, 0, 0, 5, 8],
          [0, 0, 0, 0, 0, 6],
          [0, 0, 0, 0, 0, 0]]  # 二维数组r[i][j]代表从i到j需要花的租金
-    s = [[0, 0, 0, 0, 0, 0] for i in range(0, 6)]
+
+    s = [[0, 0, 0, 0, 0, 0] for i in range(0, 6)] #辅助数组记录最优走法
+
     min = 1000  # 最小租金
     wmin = 0  # 当前最少租金
-    m = [-1, -1, -1, -1, -1, -1]
     aft = [0]  # 存放每一步后驱
-    n = len(r[0])
+    n = len(r[0])  # 停靠点数量
     temp = 0
-    for d in range(3, n + 1):  # 将问题的规模缩小，初始只有三个点需要计算，然后递增一
+    for d in range(3, n + 1):  # 将问题的规模缩小，初始只有三个停靠点需要计算，然后每次递增 1
         for i in range(0, n - d + 1):  # i计算的左边界
             j = i + d - 1  # j计算的右边界
-            for k in range(i + 1, j + 1):  # k记录i,j之间的计算值
+            for k in range(i + 1, j + 1):  # k记录i,j之间  可能存在的子问题
                 temp = r[i][k] + r[k][j]
                 if temp < r[i][j]:
-                    r[i][j] = temp
+                    r[i][j] = temp  # 如果得到当前最优值，则更新r中的最优值
                     s[i][j] = k
-                    # r[i][j] = r[i][k] + r[k][j]
 
-                    # m[i][j] = temp[0]
-
-    for i in range(0, 6):
-        for j in range(0, 6):
-            if s[i][j] <> 0:
-                print(s[i][j]),
-            else:
-                print(s[i][j]),
-        print("\n")
-
+    # for i in range(0, 6):
+    #     for j in range(0, 6):
+    #         if s[i][j] <> 0:
+    #             print(s[i][j]),
+    #         else:
+    #             print(s[i][j]),
+    #     print("\n")
+    print('最优值为'),
+    print(temp)
     print('1'),
 
 
