c69f9d7cb6d786ceceed81434c3d56ea4b00d26e
wfy815775 <815775754@qq.com>
2018-10-31 10:30:32 +0800

    两数之和

--- dynamicSelector/DYProgram.py
+++ dynamicSelector/DYProgram.py
@@ -25,11 +25,11 @@ def findKthLargest(nums, k):
 
 
 def minimumTotal(triangle):
-    if len(triangle) == 1: #极端情况  只有一层的时候 返回唯一的一个节点值
+    if len(triangle) == 1:  # 极端情况  只有一层的时候 返回唯一的一个节点值
         return triangle[0][0]
     lens = len(triangle[len(triangle) - 1])  # 最后一层的长度
 
-    r = [[0 for i in range(lens)] for i in range(lens)] #辅助数组  r[i][j]代表从0层到第i层第j节点的最优值
+    r = [[0 for i in range(lens)] for i in range(lens)]  # 辅助数组  r[i][j]代表从0层到第i层第j节点的最优值
     i, j = 1, 0  # i 代表第i层  j代表第j列
     k = 0  # 遍历每一层求出当层最优值的时候的临时变量
     for t in range(lens):
@@ -39,24 +39,36 @@ def minimumTotal(triangle):
         while j < len(triangle[i]):  # 遍历第i层的每一列
             # while k< j:
             # 遍历每一个索引只需要找到 与 它相邻的 上层索引的最优值
-            if j == 0 :         #第一个节点 上层邻接节点 只有一个
+            if j == 0:  # 第一个节点 上层邻接节点 只有一个
                 r[i][j] = triangle[i][j] + r[i - 1][j]
-            elif j == len(triangle[i]) - 1:   #最后一个节点的 上层邻接节点也只有一个
-                r[i][j] = triangle[i][j] + r[i - 1][j-1]
+            elif j == len(triangle[i]) - 1:  # 最后一个节点的 上层邻接节点也只有一个
+                r[i][j] = triangle[i][j] + r[i - 1][j - 1]
             else:
-                r[i][j] = triangle[i][j] + min(r[i - 1][j], r[i - 1][j - 1])  #中间节点的上层邻接节点 有两个 需要从两个中
-                                                                              #求出最小值
+                r[i][j] = triangle[i][j] + min(r[i - 1][j], r[i - 1][j - 1])  # 中间节点的上层邻接节点 有两个 需要从两个中
+                # 求出最小值
             j += 1
         i += 1
     return min(r[len(triangle) - 1])
 
+'''
+两数之和 同一个元素不能被重复利用
+'''
+def twoSum(nums, target):
+    dict={}
+    result =[]
+    for i in range(len(nums)):
+        if dict.get(nums[i]) != None:
+            result.append(dict.get(nums[i]))
+            result.append(i)
+        else:
+            dict[target-nums[i]] =i
+
+    return result
+
 
 if __name__ == '__main__':
-    print(minimumTotal([
-        [2],
-        [3, 4],
-        [6, 5, 7],
-        [4, 1, 8, 3]
-    ]))
+    print(twoSum([-10,-1,-18,-19],
+-19))
+
 
 
