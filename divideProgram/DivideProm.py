6de2ee3533f6065099fa705bf4967dc08c9795ee
wfy815775 <815775754@qq.com>
2018-10-31 23:03:33 +0800

    分治法

---
+++ divideProgram/DivideProm.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+'''
+给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+
+'''
+def majorityElement(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
