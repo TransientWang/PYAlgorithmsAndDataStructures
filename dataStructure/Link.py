f331bbc5e2457e7ac4df143e7b99b7ab25011a7f
wfy815775 <815775754@qq.com>
2018-11-09 09:26:52 +0800

    二叉树 将有序二叉树转换为搜索二叉树

--- dataStructure/Link.py
+++ dataStructure/Link.py
@@ -4,37 +4,51 @@
 #         self.val = x
 #         self.next = None
 from dataStructure import ListNode
+from dataStructure import TreeNode
 
 
-def addTwoNumbers(l1, l2):
-    c = 0
-    head = ListNode(0)
-    node = head
-    while l1 != None or l2 != None:
-        t1, t2, sum = 0, 0, 0
-        if l1 != None:
-            t1 = l1.val
-            l1 = l1.next
-        if l2 != None:
-            t2 = l2.val
-            l2 = l2.next
-        sum = t1 + t2 + c
+class sol(object):
 
-        if sum < 10:
-            node.val = sum
-            c = 0
-        else:
-            c = 1
-            node.val = abs(10 - sum)
 
-        if l1 != None or l2 != None:
-            node.next = ListNode(0)
-            node = node.next
-        elif c==1:
-            node.next = ListNode(1)
-            node = node.next
+    def sortedArrayToBST(self, nums):
+        if not nums:
+            return None
+        mid = len(nums) / 2
+        node = TreeNode.TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid + 1:])
+        return node
 
-    return head
+    def addTwoNumbers(l1, l2):
+        c = 0
+        head = ListNode(0)
+        node = head
+        while l1 != None or l2 != None:
+            t1, t2, sum = 0, 0, 0
+            if l1 != None:
+                t1 = l1.val
+                l1 = l1.next
+            if l2 != None:
+                t2 = l2.val
+                l2 = l2.next
+            sum = t1 + t2 + c
 
+            if sum < 10:
+                node.val = sum
+                c = 0
+            else:
+                c = 1
+                node.val = abs(10 - sum)
+
+            if l1 != None or l2 != None:
+                node.next = ListNode(0)
+                node = node.next
+            elif c == 1:
+                node.next = ListNode(1)
+                node = node.next
+
+        return head
 if __name__ == '__main__':
-    pass
+    sols = sol()
+    node = sols.sortedArrayToBST([-10, -3, 0, 5, 9])
+    print(node.val)
