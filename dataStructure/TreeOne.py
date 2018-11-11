63ce7b772ed092ad346846154b466b1ad3ab2123
wfy815775 <815775754@qq.com>
2018-11-11 23:17:23 +0800

    二叉树 二叉树的锯齿形层次遍历

--- dataStructure/TreeOne.py
+++ dataStructure/TreeOne.py
@@ -4,23 +4,48 @@
 '''
 
 from dataStructure import TreeNode
+
+
 def inorderTraversal(root):
     l = []
-    find(root,l)
+    find(root, l)
     return l
+
 
 def find(root, l):
     if not root:
         return
-    if root.left!=None:
+    if root.left != None:
         find(root.left, l)
     l.append(root.val)
-    if root.right!=None:
+    if root.right != None:
         find(root.right, l)
 
 
+def zigzagLevelOrder(root):
+    stack = [root]
+    k = 1
+    result = []
+    while len(stack) != 0:
+        i = len(stack)
+        r = []
+        while i > 0:
+            t = stack.pop(0)
+            r.append(t.val)
+            if t.left:
+                stack.append(t.left)
+            if t.right:
+                stack.append(t.right)
+            i -= 1
+        if k % 2 == 0:
+            r.reverse()
+        k += 1
+        result.append(r)
+    return result
+
+
 if __name__ == '__main__':
-        t = TreeNode.TreeNode(1)
-        t.right = TreeNode.TreeNode(2)
-        t.right.left = TreeNode.TreeNode(3)
-        print(inorderTraversal(t))
+    t = TreeNode.TreeNode(1)
+    t.right = TreeNode.TreeNode(2)
+    t.right.left = TreeNode.TreeNode(3)
+    print(zigzagLevelOrder(t))
