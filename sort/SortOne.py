15bd7353d72b179375c28e953cc41f4485caf01b
wfy815775 <815775754@qq.com>
2018-11-14 11:08:22 +0800

    排序 输出前K大元素

--- sort/SortOne.py
+++ sort/SortOne.py
@@ -1,5 +1,36 @@
 # -*- coding: UTF-8 -*-
+def swap(arr, a, b):
+    t = arr[a]
+    arr[a] = arr[b]
+    arr[b] = t
 
+
+def heapAdjust(arr, index):
+    left, right = index * 2 + 1, index * 2 + 2
+    if left < len(arr) and arr[left][0] > arr[index][0]:
+        swap(arr, left, index)
+        heapAdjust(arr, left)
+    if right < len(arr) and arr[right][0] > arr[index][0]:
+        swap(arr, right, index)
+        heapAdjust(arr, right)
+
+
+def buildHeap(arr):
+    lastIndex = int(len(arr) / 2)
+    for i in range(lastIndex):
+        index = lastIndex - 1 - i
+        heapAdjust(arr, index)
+
+
+def heapSort(arr, k):
+    buildHeap(arr)
+    res = []
+    for i in range(k):
+        t = len(arr) - i - 1
+        swap(arr, 0, len(arr) - 1)
+        res.append(arr.pop()[1])
+        heapAdjust(arr, 0)
+    return res
 
 def sortCount(nums, min, max):
     '''
@@ -27,5 +58,33 @@ def sortCount(nums, min, max):
     print(nums)
 
 
+def topKFrequent(nums, k):
+    '''
+    给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+    你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+    你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+    思路：用堆排序和hash(字典)来解决
+    先用一个字典记录每个元素出现的次数
+    再将 字典的val key转换成数组
+    用堆排序 输出 出现次数前k的key值
+    :param nums:
+    :param k:
+    :return:
+    '''
+    r = {}
+    for i in nums:
+        if r.get(i) is None:
+            r[i] = 1
+        else:
+            r[i] += 1
+    d = []
+    for key, val in r.items():
+        d.append((val, key))
+    return heapSort(d, k)
+
+
 if __name__ == '__main__':
-    sortCount([2, 0, 2, 1, 1, 0], 0, 2)
+    print(topKFrequent([1, 1, 1, 2, 2, 3],
+                       2))
+    # arr = [1, 1, 1, 2, 2, 3]
+    # heapSort(arr)
