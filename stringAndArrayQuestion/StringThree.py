6d2a734785492ffdd4c9d87569f2d481b78a3a11
wfy815775 <815775754@qq.com>
2018-12-05 11:22:37 +0800

    最小窗口子字符串

---
+++ stringAndArrayQuestion/StringThree.py
@@ -0,0 +1,41 @@
+# -*- coding: UTF-8 -*-
+
+def minWindow(s, t):
+    '''
+    最小窗口子字符串
+    给定一个字符串 S 和一个字符串 T，请在 S 中找出包含 T 所有字母的最小子串。
+
+    TODO 双指针是求解字符串问题的通用解法
+    :param s:
+    :param t:
+    :return:
+    '''
+    if len(s) < len(t) or len(s) == 0 or s is None or t is None or len(t) == 0:
+        return ""
+    start, end = 0, 0
+    cstart = 0
+    hmap = [0 for i in range(188)]  # 定义一个可以容纳所有ASCII字母的数组
+    for i in t:
+        hmap[ord(i)] += 1  # 在t串中的字符出现一次+1
+    count = 0
+    res = 2 ** 31
+
+    while end < len(s):
+        if hmap[ord(s[end])] > 0:  # 匹配count+1
+            count += 1
+        hmap[ord(s[end])] -= 1  # 尾指针遍历过得出现次数-1
+        end += 1  # 尾指针后移
+        while count == len(t):  # 找到完全匹配的字符串的时候
+            if end - start < res:  # 记录最短长度
+                res = end - start
+                cstart = start
+            if hmap[ord(s[start])] == 0:  # 如果首字符==0说明，手指针字符在t中。count-1
+                count -= 1
+            hmap[ord(s[start])] += 1  # 手指针遍历过得字符次数+1，还原（针对t中的字符）
+            start += 1  # 手指针后移
+    res = 0 if res == 2 ** 31 else res
+    return s[cstart:cstart + res]
+
+
+if __name__ == '__main__':
+    print(minWindow("aa", "aa"))
