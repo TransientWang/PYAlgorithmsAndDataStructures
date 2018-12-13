fffe7d834633898dad34bf3f29c099ffcc8531a3
wfy815775 <815775754@qq.com>
2018-12-13 10:09:49 +0800

    38.报数

--- stringAndArrayQuestion/StringOne.py
+++ stringAndArrayQuestion/StringOne.py
@@ -160,61 +160,45 @@ def strStr(haystack, needle):
     return -1
 
 
-'''
-报数序列是一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
-
-1.     1
-2.     11
-3.     21
-4.     1211
-5.     111221
-1 被读作  "one 1"  ("一个一") , 即 11。
-11 被读作 "two 1s" ("两个一"）, 即 21。
-21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
-
-给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。
-
-注意：整数顺序将表示为一个字符串。
-'''
-
-
 def countAndSay(n):
-    res = "11"
-    if n == 1:
+    '''
+    38.报数
+    报数序列是一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
+
+    1.     1
+    2.     11
+    3.     21
+    4.     1211
+    5.     111221
+    1 被读作  "one 1"  ("一个一") , 即 11。
+    11 被读作 "two 1s" ("两个一"）, 即 21。
+    21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
+
+    给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。
+
+    注意：整数顺序将表示为一个字符串。
+    '''
+    if n ==1:
         return "1"
-    elif n == 2:
-        return "11"
-    newres = ""
-    h = 0
-    x = 3
-    while x <= n:
-
-        while h < len(res) - 1:
-            k = 1
-            while h < len(res) - 1 and res[h] == res[h + 1]:
-                h += 1
-                k += 1
-            newres += str(k)
-            newres += res[h]
-
-            h += 1
-            if h == len(res) - 1:
-                newres += str(1)
-                newres += res[h]
-
-        res = newres
-        newres = ""
-        x += 1
-        h = 0
-    return res
-
-
-'''
-寻找最长公共前缀
-'''
-
+    else:
+        data = countAndSay(n - 1)
+        res = ""
+        curr = data[0]
+        time = 1
+        for i in data[1:]:
+            if i == curr:
+                time += 1
+            else:
+                res += str(time) + curr
+                curr = i
+                time = 1
+        res += str(time) + curr
+        return res
 
 def longestCommonPrefix(strs):
+    '''
+    寻找最长公共前缀
+    '''
     r = len(strs)
     if r == 0:
         return ""
@@ -244,4 +228,4 @@ def longestCommonPrefix(strs):
 
 if __name__ == '__main__':
     # print(isPalindrome("A man, a plan, a canal: Panama"))
-    print((reverse(-123)))
+    print((countAndSay(4)))
