beb3a1414d991c43740c7fb1ba24e0850b502e0c
wfy815775 <815775754@qq.com>
2018-12-15 10:29:27 +0800

    98.验证二叉搜索树

--- dataStructure/DSCodeTwo.py
+++ dataStructure/DSCodeTwo.py
@@ -36,5 +36,48 @@ def copyRandomList(head):
     return res
 
 
+def maxDepth(root):
+    '''
+    141.二叉树的最大深度
+    :param self:
+    :param root:
+    :return:
+    '''
+    deepth = 1
+    queue = [root]
+    while len(queue) != 0:
+        deepth += 1
+        lens = len(queue)
+        for i in range(lens):
+            t = queue.pop(0)
+            if t.left is not None:
+                queue.append(t.left)
+            if t.right is not None:
+                queue.append(t.right)
+
+    return deepth
+
+
+
+def isValidBST(root):
+    '''
+    TODO 再看
+    98.验证二叉搜索树
+    思路：类似DFS,保存上一个节点的值作为最大值或者最小值，分别向两边搜索。
+    :param root:
+    :return:
+    '''
+
+    def validBST(self, root, small, large):
+        if root == None:
+            return True
+        if small >= root.val or large <= root.val:
+            return False
+        return self.validBST(root.left, small, root.val) and self.validBST(root.right, root.val, large)
+
+    return validBST(root, -2 ** 32, 2 ** 32 - 1)
+
+
 if __name__ == '__main__':
-    pass
+
+    isValidBST(None)
