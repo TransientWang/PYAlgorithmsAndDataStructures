ed9c6e3bd8579835fa2745a79a8cd07a8bc6906c
wfy815775 <815775754@qq.com>
2018-12-16 14:49:20 +0800

    190	颠倒二进制位

--- mathquestion/MathQuestionOne.py
+++ mathquestion/MathQuestionOne.py
@@ -45,9 +45,9 @@ def countPrimes(n):  # TODO 应该再复习
     bool = [True for i in range(n - 1)]
     bool[0] = False
     sqrts = math.ceil(math.sqrt(n))
-    for i in range(2, int(sqrts)): # 从2到根号n向下取整
+    for i in range(2, int(sqrts)):  # 从2到根号n向下取整
         if bool[i - 1]:
-            for j in range(i * i, n, i):  #i的倍数全部都标记
+            for j in range(i * i, n, i):  # i的倍数全部都标记
                 bool[j - 1] = False
     res = 0
     for i in range(n - 1):
@@ -55,160 +55,186 @@ def countPrimes(n):  # TODO 应该再复习
             res += 1
     return res
 
-    def isPowerOfThree(n):
-        if n <= 0:
+
+def isPowerOfThree(n):
+    if n <= 0:
+        return False
+    maxint = 0x7fffffff
+    k = int(math.log(maxint, 3))
+
+    b = 3 ** k
+
+    return b % n == 0
+
+
+def romanToInt(s):
+    '''
+    罗马数字转换
+    '''
+    map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+    for i in range(len(s)):
+        if i == 0 or map[s[i]] <= map[s[i - 1]]:
+            result += map[s[i]]
+        else:
+            result = result + map[s[i]] - 2 * map[s[i - 1]]
+    return result
+
+
+def hammingWeight(n):
+    '''
+    191.位1的个数
+    '''
+    # new_n = bin(n)[2:].zfill(31)
+    # print(new_n)
+    # new_n = list(new_n)
+    # return new_n.count('1')
+    if n == 0:
+        return 0
+    res = 1
+    while n > 2:
+        res += n % 2
+        n = math.floor(n / 2)
+    return res
+
+
+def reverseBits(n):
+    '''
+    190.颠倒二进制位
+    依次将n的末尾位给result的末尾位，并且result向左移31次
+    '''
+    # l = list('{0:032b}'.format(n))
+    # l = list(map(int, l))
+    # l.reverse()
+    # l = list(map(str, l))
+    # s = ''.join(l)
+    # return int(s, 2)
+    res = 0
+    for i in range(32):
+        tmp = n & 1
+        n = n >> 1
+        res = (res << 1) | tmp
+    return res
+
+
+def generate(numRows):
+    '''
+    帕斯卡三角形
+    用两个数组保存临时结果
+    数组长度比层数多2
+    两边置0方便计算
+    '''
+    if numRows == 0:
+        return []
+    if numRows == 1:
+        return [[1]]
+    res = [[1], [1, 1]]
+    dp = [0 for i in range(numRows + 2)]
+    new_dp = [0 for i in range(numRows + 2)]
+    dp[1] = 1
+    dp[2] = 1
+    for j in range(2, numRows):
+        for k in range(1, j + 2):
+            new_dp[k] = dp[k] + dp[k - 1]
+        dp = new_dp[:]
+        res.append(new_dp[1:j + 2])
+    return res
+
+
+def isValid(s):
+    '''
+    有效的括号
+    用一个栈来保存对应的左括号，如果遇到右括号的时候从栈中弹出一个
+    因为是按顺序排列的左括号 弹出来的一定是相应的右括号
+    如果 不相等 就说明 括号没有对称
+    '''
+    dp = []
+    for i in s:
+        if i == "(":
+            dp.append(")")
+        elif i == "{":
+            dp.append("}")
+        elif i == "[":
+            dp.append("]")
+        elif not dp or dp.pop() != i:
             return False
-        maxint = 0x7fffffff
-        k = int(math.log(maxint, 3))
+    return not dp
 
-        b = 3 ** k
 
-        return b % n == 0
-
-    def romanToInt(s):
-        '''
-        罗马数字转换
-        '''
-        map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        result = 0
-        for i in range(len(s)):
-            if i == 0 or map[s[i]] <= map[s[i - 1]]:
-                result += map[s[i]]
-            else:
-                result = result + map[s[i]] - 2 * map[s[i - 1]]
-        return result
-
-    def hammingWeight(n):
-        '''
-         位1的个数
-        '''
-        new_n = bin(n)[2:].zfill(31)
-        print(new_n)
-        new_n = list(new_n)
-        return new_n.count('1')
-
+def missingNumber(nums):
     '''
-    颠倒二进制位
+    缺失的数字
     '''
+    t = 0
+    tmp = 0
+    for i in range(len(nums)):
+        t += i
+        tmp += nums[i]
+    t += len(nums)
 
-    def reverseBits(n):
-        l = list('{0:032b}'.format(n))
-        l = list(map(int, l))
-        l.reverse()
-        l = list(map(str, l))
-        s = ''.join(l)
-        return int(s, 2)
+    return t - tmp
 
-    def generate(numRows):
-        '''
-        帕斯卡三角形
-        用两个数组保存临时结果
-        数组长度比层数多2
-        两边置0方便计算
-        '''
-        if numRows == 0:
-            return []
-        if numRows == 1:
-            return [[1]]
-        res = [[1], [1, 1]]
-        dp = [0 for i in range(numRows + 2)]
-        new_dp = [0 for i in range(numRows + 2)]
-        dp[1] = 1
-        dp[2] = 1
-        for j in range(2, numRows):
-            for k in range(1, j + 2):
-                new_dp[k] = dp[k] + dp[k - 1]
-            dp = new_dp[:]
-            res.append(new_dp[1:j + 2])
-        return res
 
-    def isValid(s):
-        '''
-        有效的括号
-        用一个栈来保存对应的左括号，如果遇到右括号的时候从栈中弹出一个
-        因为是按顺序排列的左括号 弹出来的一定是相应的右括号
-        如果 不相等 就说明 括号没有对称
-        '''
-        dp = []
-        for i in s:
-            if i == "(":
-                dp.append(")")
-            elif i == "{":
-                dp.append("}")
-            elif i == "[":
-                dp.append("]")
-            elif not dp or dp.pop() != i:
-                return False
-        return not dp
+'''
+颠倒二进制位
+'''
 
-    def missingNumber(nums):
-        '''
-        缺失的数字
-        '''
-        t = 0
-        tmp = 0
-        for i in range(len(nums)):
-            t += i
-            tmp += nums[i]
-        t += len(nums)
 
-        return t - tmp
+def hammingDistance(x, y):
+    l1 = list('{0:032b}'.format(x))
+    l1 = list(map(int, l1))
+    l2 = list('{0:032b}'.format(y))
+    l2 = list(map(int, l2))
 
-    '''
-    颠倒二进制位
-    '''
+    res = 0
 
-    def hammingDistance(x, y):
-        l1 = list('{0:032b}'.format(x))
-        l1 = list(map(int, l1))
-        l2 = list('{0:032b}'.format(y))
-        l2 = list(map(int, l2))
+    for i in range(32):
+        if l1[i] != l2[i]:
+            res += 1
 
-        res = 0
+    return res
 
-        for i in range(32):
-            if l1[i] != l2[i]:
-                res += 1
 
-        return res
+'''
+Shuffle an Array
+打乱一个没有重复元素的数组。
+'''
 
-    '''
-    Shuffle an Array
-    打乱一个没有重复元素的数组。
-    '''
 
-    class Solution(object):
+class Solution(object):
 
-        def __init__(self, nums):
-            self.nums = nums
-            """
-            :type nums: List[int]
-            """
+    def __init__(self, nums):
+        self.nums = nums
+        """
+        :type nums: List[int]
+        """
 
-        def reset(self):
-            return self.nums
-            """
-            Resets the array to its original configuration and return it.
-            :rtype: List[int]
-            """
+    def reset(self):
+        return self.nums
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
 
-        def shuffle(self):
-            """
-                   Returns a random shuffling of the array.
-                   :rtype: List[int]
-                   """
-            r = []
-            tmp = self.nums[:]
-            while tmp:
-                ran = int(random.uniform(0, len(tmp)))
-                r.append(tmp[ran])
-                tmp.remove(tmp[ran])
-            return r
+    def shuffle(self):
+        """
+               Returns a random shuffling of the array.
+               :rtype: List[int]
+               """
+        r = []
+        tmp = self.nums[:]
+        while tmp:
+            ran = int(random.uniform(0, len(tmp)))
+            r.append(tmp[ran])
+            tmp.remove(tmp[ran])
+        return r
 
-    def sortColors(nums):
-        nums.sort()
-        return nums
+
+def sortColors(nums):
+    nums.sort()
+    return nums
 
 
 if __name__ == '__main__':
-    print(countPrimes(10))
+    print(0x01)
+    pass
