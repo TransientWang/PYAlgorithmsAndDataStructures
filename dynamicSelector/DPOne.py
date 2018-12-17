a3dd66216a37217362afd5f266d1db4909f548ba
wfy815775 <815775754@qq.com>
2018-12-17 11:19:32 +0800

    322.零钱兑换

--- dynamicSelector/DPOne.py
+++ dynamicSelector/DPOne.py
@@ -5,25 +5,22 @@ import copy
 
 def coinChange(coins, amount):
     '''
-    零钱兑换
+    322.零钱兑换
     给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
     如果没有任何一种硬币组合能组成总金额，返回 -1。
     我们需要尽可能少的硬币个数，所以从11的总金额中取出任意一枚硬币，剩下的金额所需最少硬币个数再加上1就是所需硬币个数，
     即所需硬币个数为：dp[10]+1、dp[9]+1、dp[6]+1，再从中取最小值，即可求解。以此类推，dp[10]=min(dp[9]+1,dp[8]+1,dp[5]+1)
     所以可以看出，通过求出所有dp[1]、dp[2]、...、dp[10]的值，最终就能得到dp[11]的值。
     '''
-    dp = [0]  # dp[i]代表在有多少钱的的时候，最少能找零的硬币数量
-    l = amount + 1
-    for i in range(1, l):
-        dp.append(l)
+
+    dp = [amount + 1 for i in range(amount + 1)]
+    dp[0] = 0
+    for i in range(1, amount + 1):
         for j in coins:
             if i >= j:
-                dp[i] = min(dp[i], dp[i - j] + 1)
-    print(dp)
-    if dp[amount] == amount + 1:
-        return -1
-    else:
-        return dp[amount]
+                dp[i] = min(dp[i - j] + 1, dp[i])
+
+    return dp[-1] if dp[-1] != amount + 1 else -1
 
 
 '''
@@ -226,5 +223,6 @@ def maxProfitOne(prices):
 
 
 if __name__ == '__main__':
-    print(maxProfitOne([1, 2, 3, 0, 2]))
+    print(coinChange([1],
+0))
     # [1,3,6,7,9,4,10,5,6]
