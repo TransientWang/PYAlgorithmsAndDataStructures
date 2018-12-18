38eb2272218bf86cb5301a48cfeb4b6db98d7724
wfy815775 <815775754@qq.com>
2018-12-18 15:30:59 +0800

    334.递增的三元子序列

--- stringAndArrayQuestion/StringTwo.py
+++ stringAndArrayQuestion/StringTwo.py
@@ -214,11 +214,10 @@ def longestPalindrome(s):
 
 def increasingTriplet(nums):
     '''
-     递增的三元子序列
+     334.递增的三元子序列
     给定一个未排序的数组，判断这个数组中是否存在长度为 3 的递增子序列。
-
+    贪心选择
     数学表达式如下:
-
     如果存在这样的 i, j, k,  且满足 0 ≤ i < j < k ≤ n-1，
     使得 arr[i] < arr[j] < arr[k] ，返回 true ; 否则返回 false 。
     '''
