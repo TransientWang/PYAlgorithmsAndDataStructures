446f07d0f543f85e887751792b9e3f79d69dbe31
wfy815775 <815775754@qq.com>
2018-12-24 11:44:28 +0800

    166.分数化小数

--- mathquestion/MathQuestionThree.py
+++ mathquestion/MathQuestionThree.py
@@ -2,7 +2,7 @@
 
 def fractionToDecimal(numerator, denominator):
     '''
-    分数化小数
+    166.分数化小数
     出现相同被除数就证明出现循环
     :param numerator:
     :param denominator:
@@ -10,30 +10,26 @@ def fractionToDecimal(numerator, denominator):
     '''
     if numerator == 0:
         return "0"
-    symbol = 1 if numerator * denominator > 0 else -1  # 符号
+    bool = (numerator > 0) ^ (denominator > 0)
     numerator = abs(numerator)
     denominator = abs(denominator)
-    hmap = {}
-    result = str(int(numerator / denominator))  # 整数部分
-    remainder = numerator % denominator  # 余数，也是下一次计算的被除数
-    if remainder != 0:
-        result += "."
+    dmap = {}
+    result = numerator // denominator
+    remainder = numerator % denominator
+    result = str(result) if remainder == 0 else str(result) + "."
     index = len(result) - 1
     while remainder != 0:
-        if hmap.get(remainder, -1) != -1:
-            t = hmap.get(remainder) + 1
+        if remainder in dmap.keys():
+            t = dmap[remainder] + 1
             result = result[:t] + "(" + result[t:] + ")"
             break
         else:
-            hmap[remainder] = index
+            dmap[remainder] = index
             index += 1
-
         remainder *= 10
-        result += str(remainder // denominator)
+        result += str((remainder // denominator))
         remainder = remainder % denominator
-    if symbol < 0:
-        result = "-" + result
-    return result
+    return "-" + result if bool else result
 
 
 def isPalindrome(x):
@@ -51,4 +47,4 @@ def isPalindrome(x):
 
 
 if __name__ == '__main__':
-    print(isPalindrome(-121))
+    print(fractionToDecimal(2,3))
