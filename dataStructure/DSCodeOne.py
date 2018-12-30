3c53fb3786dad6fa8358e7c0400384f0a90deb1d
wfy815775 <815775754@qq.com>
2018-12-30 10:59:11 +0800

    547.Friend Circles

--- dataStructure/DSCodeOne.py
+++ dataStructure/DSCodeOne.py
@@ -4,7 +4,7 @@ from dataStructure import TreeNode
 
 def maxPathSum(root):
     '''
-    124.二叉树中的最大路径和
+    findCircleNum
     DFS
     给定一个非空二叉树，返回其最大路径和。
 
@@ -38,6 +38,7 @@ def maxPathSum(root):
 
 def findCircleNum(M):
     '''
+    547.Friend Circles
     班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有是传递性。如果已知 A 是 B 的朋友，B 是 C 的朋友，那么我们可以认为 A 也是 C 的朋友。所谓的朋友圈，是指所有朋友的集合。
 
     给定一个 N * N 的矩阵 M，表示班级中学生之间的朋友关系。如果M[i][j] = 1，表示已知第 i 个和 j 个学生互为朋友关系，否则为不知道。你必须输出所有学生中的已知的朋友圈总数。
@@ -48,26 +49,22 @@ def findCircleNum(M):
     :param M:
     :return:
     '''
-    pass
     if M is None or len(M) == 0:
         return 0
     row = len(M)
-    colum = len(M[0])
-    visit = [False for i in range(row)]
-
-    count = 0
+    visited = [False for i in range(row)]
 
     def find(x):
-        for y in range(colum):
-            if M[x][y] == 1 and visit[y] == 0:
-                visit[y] = True
+        for y in range(row):
+            if M[x][y] == 1 and visited[y] == False:
+                visited[y] = True
                 find(y)
 
+    count = 0
     for i in range(row):
-        if visit[i] == False:
+        if visited[i] == False:
             find(i)
             count += 1
-
     return count
 
 
