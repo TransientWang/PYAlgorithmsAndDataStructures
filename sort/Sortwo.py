734e7bf72a95cc1c1e3927b92e3206337760d8f0
wfy815775 <815775754@qq.com>
2019-01-04 13:40:05 +0800

    4.寻找两个有序数组的中位数

--- sort/Sortwo.py
+++ sort/Sortwo.py
@@ -37,7 +37,7 @@ def wiggleSortOne(nums):
 
 def findMedianSortedArrays(nums1, nums2):
     '''
-    寻找两个有序数组的中位数
+    4.寻找两个有序数组的中位数
     给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
 
     请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
@@ -50,34 +50,42 @@ def findMedianSortedArrays(nums1, nums2):
     :param nums2:
     :return:
     '''
-    lenOne = len(nums1)
-    lenTwo = len(nums2)
-    flag = True
-    mid = 0
-    if (lenOne + lenTwo) % 2 == 0:
-        flag = False
-    mid = (lenOne + lenTwo) // 2
-    tmp = []
-    i, j = 0, 0
-    while i < lenOne and j < lenTwo and mid >= 0:
-        if nums1[i] <= nums2[j]:
-            tmp.append(nums1[i])
-            i += 1
-        else:
-            tmp.append(nums2[j])
-            j += 1
-        mid -= 1
-    while mid >= 0 and i < lenOne:
-        tmp.append(nums1[i])
-        i += 1
-        mid -= 1
-    while mid >= 0 and j < lenTwo:
-        tmp.append(nums2[j])
-        j += 1
-        mid -= 1
-    print(tmp)
-    t = tmp[-1] if flag else (tmp[-1] + tmp[-2]) / 2
-    return t
+    # lenOne = len(nums1)
+    # lenTwo = len(nums2)
+    # flag = True
+    # mid = 0
+    # if (lenOne + lenTwo) % 2 == 0:
+    #     flag = False
+    # mid = (lenOne + lenTwo) // 2
+    # tmp = []
+    # i, j = 0, 0
+    # while i < lenOne and j < lenTwo and mid >= 0:
+    #     if nums1[i] <= nums2[j]:
+    #         tmp.append(nums1[i])
+    #         i += 1
+    #     else:
+    #         tmp.append(nums2[j])
+    #         j += 1
+    #     mid -= 1
+    # while mid >= 0 and i < lenOne:
+    #     tmp.append(nums1[i])
+    #     i += 1
+    #     mid -= 1
+    # while mid >= 0 and j < lenTwo:
+    #     tmp.append(nums2[j])
+    #     j += 1
+    #     mid -= 1
+    # print(tmp)
+    # t = tmp[-1] if flag else (tmp[-1] + tmp[-2]) / 2
+    # return t
+
+    num = nums1 + nums2
+    num.sort()
+    mid = (len(num) - 1) // 2
+    if len(num) & 0x1:
+        return num[mid]
+    else:
+        return (num[mid] + num[mid + 1]) / 2
 
 
 def kthSmallest(matrix, k):
@@ -88,6 +96,7 @@ def kthSmallest(matrix, k):
     :param k:
     :return:
     '''
+
     def getMid(mid):
         """
         二分法，从左下角开始
@@ -103,7 +112,7 @@ def kthSmallest(matrix, k):
             if matrix[i][j] > mid:
                 i -= 1
             else:
-                res += i + 1   # 每次向右移动一位，该值竖直方向向上（包括该值）的i+1位都一定小于预期值
+                res += i + 1  # 每次向右移动一位，该值竖直方向向上（包括该值）的i+1位都一定小于预期值
                 j += 1
         return res
 
@@ -135,8 +144,4 @@ def getMid(matrix, mid):
 
 
 if __name__ == '__main__':
-    print(getMid([
-        [1, 5, 9],
-        [10, 11, 13],
-        [12, 13, 15]
-    ], 13))
+    print(findMedianSortedArrays([1, 2], [3, 4]))
