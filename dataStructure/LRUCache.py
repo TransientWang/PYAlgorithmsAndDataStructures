2d508dbaa1167724f5095aea794e3fc51e07268a
wfy815775 <815775754@qq.com>
2019-01-04 15:09:46 +0800

    146.LRU缓存机制

--- dataStructure/LRUCache.py
+++ dataStructure/LRUCache.py
@@ -3,7 +3,8 @@ class LRUCache:
 
     def __init__(self, capacity):
         """
-        :type capacity: int
+        146.LRU缓存机制
+        acity: int
         """
         self.hash = {}
         self.cur = capacity
@@ -28,26 +29,22 @@ class LRUCache:
         :rtype: void
         """
         if self.hash.get(key) is None:
-            if self.cur == 0:
+            if self.cur == 0:  # cur减少到0的时候就开始往外弹过期的
                 self.hash.pop(self.list.pop())
                 self.list.insert(0, key)
             else:
-                self.cur -= 1
+                self.cur -= 1  # cur不为0就减1
                 self.list.insert(0, key)
-        else:
+        else:  # 已存在
             self.list.remove(key)
             self.list.insert(0, key)
         self.hash[key] = value
 
 
-
-
-
 if __name__ == '__main__':
-    cache = LRUCache(2)
-    cache.put(2,1)
-    cache.put(1,1)
-    cache.put(2,3)
-    cache.put(4,1)
+    cache = LRUCache(1)
+    cache.put(2, 1)
+    cache.put(1, 1)
+    cache.put(2, 3)
+    cache.put(4, 1)
     print(cache.get(1))
-    print(cache.get(2))
