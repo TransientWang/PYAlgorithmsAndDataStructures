47d8a4c81b2a9748fd5284bc0325adc2ea0a9ef8
wfy815775 <815775754@qq.com>
2019-01-23 11:17:10 +0800

    99. 恢复二叉搜索树

--- dataStructure/DSCodeFour.py
+++ dataStructure/DSCodeFour.py
@@ -64,6 +64,96 @@ def generateTrees(n):
     return find(1, n)
 
 
+def recoverTree(root):
+    """
+    99. 恢复二叉搜索树
+    先中序遍历，在与排序的列表比较，找出错误的一对
+    然后再遍历序列然后交换
+    :type root: TreeNode
+    :rtype: void Do not return anything, modify root in-place instead.
+    """
+    res = []
+
+    def inorder(root):
+        """
+        中序遍历
+        :param root:
+        :return:
+        """
+        if not root:
+            return []
+        inorder(root.left)
+        res.append(root.val)
+        inorder(root.right)
+
+    def swap(root):
+        """
+        遍历并交换
+        :param root:
+        :return:
+        """
+        if not root:
+            return
+        swap(root.left)
+        if root.val == diff[0]:
+            root.val = diff[1]
+        elif root.val == diff[1]:
+            root.val == diff[0]
+        swap(root.right)
+
+    inorder(root)
+
+    nodes = res[:]
+    nodes.sort()
+    diff = []
+
+    for i in range(len(res)):
+        if nodes[i] != res[i]:
+            diff.append(nodes[i])
+
+    swap(root)
+
+
+def recoverTreeOne(root):
+    """
+    99. 恢复二叉搜索树
+    :type root: TreeNode
+    :rtype: void Do not return anything, modify root in-place instead.
+    """
+    first, second, prev = None, None, None
+    pred = None  # rightmost node in left tree
+
+    cur = root
+    while cur:
+        # for each node, we compare it with prev node as we did in in-order-traversal
+        if prev and cur.val < prev.val:
+            if not first:
+                first = prev
+            # we may have corner case that two incorrect nodes are in same pair.
+            # so we assign second with a new node at each time
+            second = cur
+        if cur.left:  # got left tree, then let's locate its rightmost node in left tree
+            pred = cur.left
+            # we may have visited the left tree before,
+            # and connect the rightmost node with curr node (root node)
+            while pred.right and pred.right != cur:
+                pred = pred.right
+            if pred.right == cur:  # this condition happens if and only if we have visited left tree before
+                # if this left tree has been visited before, then we are done with it
+                # cut the connection with currNode and start visit curr's right tree
+                pred.right = None
+                prev = cur
+                cur = cur.right
+            else:
+                # if this left tree has not been visited before,
+                # then we create a back edge from rightmost node to curr node,
+                # so we can return to the start point after done the left tree
+                pred.right = cur
+                cur = cur.left
+        else:  # no left tree, then just visit its right tree
+            prev = cur
+            cur = cur.right
+    first.val, second.val = second.val, first.val
 
 
 if __name__ == '__main__':
