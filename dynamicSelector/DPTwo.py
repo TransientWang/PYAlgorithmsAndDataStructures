290c16af64b60ce80869e69d04ad5079998d8dae
wfy815775 <815775754@qq.com>
2019-01-23 10:22:05 +0800

    95. 不同的二叉搜索树 II

--- dynamicSelector/DPTwo.py
+++ dynamicSelector/DPTwo.py
@@ -89,5 +89,27 @@ def numTrees(n):
     return dp[-1]
 
 
+def isInterleave(s1, s2, s3):
+    """
+    97. 交错字符串
+    思路：dp[i][j] 代表 s1[i] 和 s2[j] 之后的子串是否可以组成 s3[i+j]之后的子串
+    :type s1: str
+    :type s2: str
+    :type s3: str
+    :rtype: bool
+    """
+    if len(s1) + len(s2) != len(s3):
+        return False
+    dp = [[False for i in range(len(s2) + 1)] for i in range(len(s1) + 1)]
+    dp[-1][-1] = True
+    for i in range(len(s1), -1, -1):
+        for j in range(len(s2), -1, -1):
+            if i < len(s1) and s1[i] == s3[i + j] and dp[i + 1][j]:  #如果s1[i]满足条件并且 s2[i+j+1] 之前的子串可满足
+                dp[i][j] = True
+            elif j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
+                dp[i][j] = True
+    return dp[0][0]
+
+
 if __name__ == '__main__':
-    print(numDecodings("17"))
+    print(isInterleave(s1="aabcc", s2="dbbca", s3="aadbbcbcac"))
