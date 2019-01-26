724b292c47050611f660cdacc14cccbd17b76b2f
wfy815775 <815775754@qq.com>
2019-01-26 13:46:50 +0800

    132. 分割回文串 II

--- dynamicSelector/DPThree.py
+++ dynamicSelector/DPThree.py
@@ -17,15 +17,17 @@ def minCut(s):
     #         if s[j:i] == s[j:i][::-1]:
     #             mem[i] = min(mem[i], mem[j] + 1)
     # return mem[-1]
-
-    dp = [i for i in range(len(s))]
-    vald = [[False for i in range(len(s))] for i in range(len(s))]
+    if s == s[::-1]: return 0
+    for i in range(1, len(s)):
+        if s[i:] == s[:i - 1:-1] and s[:i] == s[i - 1::-1]: return 1
+    # 切分 0次 和 1次的过滤一下
+    dp = [i for i in range(len(s))]  # 0到dp[i]需要切分几次
+    vald = [[False for i in range(len(s))] for i in range(len(s))]  # 从s[i][j]是否是回文串
     for i in range(len(s)):
         for j in range(i + 1):  # 遍历所有子序列
             if s[i] == s[j] and (i - j <= 1 or vald[j + 1][i - 1]):
                 dp[i] = min(dp[i], dp[j - 1] + 1) if j != 0 else 0  # j == 0 的时候不用切分，j !=0 的时候需要进行比较
                 vald[j][i] = True
-
     return dp[-1]
 
 
