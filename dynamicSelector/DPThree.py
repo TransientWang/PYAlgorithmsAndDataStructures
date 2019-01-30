1ef4ef8793018941e4f9a9be12899907c9a21ac6
wfy815775 <815775754@qq.com>
2019-01-30 13:43:07 +0800

    174. 地下城游戏

--- dynamicSelector/DPThree.py
+++ dynamicSelector/DPThree.py
@@ -31,5 +31,32 @@ def minCut(s):
     return dp[-1]
 
 
+def calculateMinimumHP(dungeon):
+    """
+    174. 地下城游戏
+    动态规划反推
+    :type dungeon: List[List[int]]
+    :rtype: int
+    """
+    m = len(dungeon)
+    n = len(dungeon[0])
+    dp = [[0] * n] * m
+
+    for i in range(m - 1, -1, -1):
+        for j in range(n - 1, -1, -1):
+            if i == m - 1 and j == n - 1:  # 边界值
+                dp[i][j] = max(1, 1 - dungeon[i][j])
+            elif i == m - 1:
+                print(dp[i][j + 1] - dungeon[i][j])
+                dp[i][j] = max(1, dp[i][j + 1] - dungeon[i][j])
+
+            elif j == n - 1:
+                dp[i][j] = max(1, dp[i + 1][j] - dungeon[i][j])
+
+            else:
+                dp[i][j] = max(1, min(dp[i + 1][j] - dungeon[i][j], dp[i][j + 1] - dungeon[i][j]))
+    return dp[0][0]
+
+
 if __name__ == '__main__':
-    print(minCut("abbab"))
+    print(calculateMinimumHP([[2, -1, 2]]))
