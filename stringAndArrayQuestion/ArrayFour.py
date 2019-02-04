9f79c169a206da421ee0f1a8f88a7b1acc784052
wfy815775 <815775754@qq.com>
2019-02-04 13:05:50 +0800

    220. 存在重复元素 III

--- stringAndArrayQuestion/ArrayFour.py
+++ stringAndArrayQuestion/ArrayFour.py
@@ -21,5 +21,45 @@ def combinationSum3(k, n):
     return res
 
 
+def containsNearbyAlmostDuplicate(nums, k, t):
+    """
+    220. 存在重复元素 III
+    滑动窗口
+    :type nums: List[int]
+    :type k: int
+    :type t: int
+    :rtype: bool
+    """
+    # n = len(nums)
+    # if t == 0 and n == len(set(nums)):
+    #     return False
+    #
+    # for i in range(len(nums)):
+    #     j = i + 1
+    #     while j < len(nums) and j - i <= k:
+    #         t1 = nums[i]
+    #         t2 = nums[j]
+    #         if abs(t1 - t2) <= t:
+    #             return True
+    #         j += 1
+    # return False
+    lenth = len(nums)
+    a = set()  # set集合，是一个无序且不重复的元素集合。
+    for i in range(lenth):
+        if t == 0:
+            if nums[i] in a:
+                return True
+        else:
+            for atem in a:
+                if abs(nums[i] - atem) <= t:
+                    return True
+        a.add(nums[i])
+        if len(a) == k + 1:
+            a.remove(nums[i - k])
+    return False
+
+
 if __name__ == '__main__':
-    print(combinationSum3(3, 9))
+    print(containsNearbyAlmostDuplicate([7, 2, 8],
+                                        2,
+                                        1))
