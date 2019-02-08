e3d5835a3ecc6ebf3bc3ee132cf7e80fef6b0207
wfy815775 <815775754@qq.com>
2019-02-08 11:47:21 +0800

    229. 求众数 II

--- stringAndArrayQuestion/ArrayFour.py
+++ stringAndArrayQuestion/ArrayFour.py
@@ -159,6 +159,33 @@ def majorityElement(nums):
             res.append(k)
     return res
 
+    # 多数投票/有超过 1/3 的元素结果数量一定小于等于两个
+    num1 = num2 = 1e9
+    cnt1 = cnt2 = 0
+    for num in nums:
+        if num == num1:
+            cnt1 += 1
+        elif num == num2:
+            cnt2 += 1
+        elif cnt1 == 0:
+            num1 = num
+            cnt1 += 1
+        elif cnt2 == 0:
+            num2 = num
+            cnt2 += 1
+        else:
+            cnt1 -= 1
+            cnt2 -= 1
+    # 找出个数
+    cnt1 = nums.count(num1)
+    cnt2 = nums.count(num2)
+    res = []
+    if cnt1 > int(len(nums) / 3):
+        res.append(num1)
+    if cnt2 > int(len(nums) / 3):
+        res.append(num2)
+    return res
+
 
 if __name__ == '__main__':
     print(majorityElement([1, 1]))
