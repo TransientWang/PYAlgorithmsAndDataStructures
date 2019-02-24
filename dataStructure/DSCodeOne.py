cbb3c67062b2175d63b2a1fcfda88556761b7521
wfy815775 <815775754@qq.com>
2019-02-24 12:20:45 +0800

    547. 朋友圈(review)

--- dataStructure/DSCodeOne.py
+++ dataStructure/DSCodeOne.py
@@ -50,10 +50,8 @@ def maxPathSum(root):
 
 def findCircleNum(M):
     '''
-    547.Friend Circles
-    班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有是传递性。如果已知 A 是 B 的朋友，B 是 C 的朋友，那么我们可以认为 A 也是 C 的朋友。所谓的朋友圈，是指所有朋友的集合。
-
-    给定一个 N * N 的矩阵 M，表示班级中学生之间的朋友关系。如果M[i][j] = 1，表示已知第 i 个和 j 个学生互为朋友关系，否则为不知道。你必须输出所有学生中的已知的朋友圈总数。
+    547. 朋友圈(review)
+    只要两两认识，就可以成为朋友圈
     DFS:维护一个已经遍历过得人的数组
     如果这个人没有遍历过，就标记已遍历，然后在图中搜索这一行中为1 ，且还没有遍历过得人，
     找到后继续对这个人标记，然后递归搜索
@@ -61,22 +59,18 @@ def findCircleNum(M):
     :param M:
     :return:
     '''
-    if M is None or len(M) == 0:
-        return 0
-    row = len(M)
-    visited = [False for i in range(row)]
-
+    dp = [False] * len(M)
     def find(x):
-        for y in range(row):
-            if M[x][y] == 1 and visited[y] == False:
-                visited[y] = True
-                find(y)
+        for i in range(len(M)):
+            if M[x][i] == 1 and dp[i] == False:
+                dp[i] = True
+                find(i)
 
     count = 0
-    for i in range(row):
-        if visited[i] == False:
-            find(i)
+    for i in range(len(M)):
+        if dp[i] == False:
             count += 1
+            find(i)
     return count
 
 
