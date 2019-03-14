7ca9f46342e6a6bc2b17d1a1d96cdcd4daaf25cc
wfy815775 <815775754@qq.com>
2019-03-14 16:22:57 +0800

    0-1 背包问题

--- nowcoder/bagSlution.py
+++ nowcoder/bagSlution.py
@@ -13,14 +13,17 @@ def bagSlution(c, w, v):
     dp = [[0] * (c + 1) for i in range(len(w) + 1)]
     res = 0
     for i in range(1, len(w) + 1):
+        if w[i - 1] > c:
+            dp[i] = dp[i - 1]
         for j in range(w[i - 1], c + 1):
             dp[i][j] = max(dp[i - 1][j - w[i - 1]] + v[i - 1], dp[i - 1][j])
-            res = max(dp[i - 1][j], res)
+            res = max(dp[i][j], res)
+
     for i in range(len(w) + 1):
         print(dp[i])
     print(res)
 
 
 if __name__ == '__main__':
-    bagSlution(12, [4, 6, 2, 2, 5, 1],
-               [8, 10, 6, 3, 7, 2])
+    bagSlution(8, [3, 2, 9, 4, 3],
+               [700, 500, 800, 600, 520])
