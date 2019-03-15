69b53f4a05fcd1c968056aca9c2f7ddc515e8809
wfy815775 <815775754@qq.com>
2019-03-15 13:37:40 +0800

    10. 正则表达式匹配(review)

--- dynamicProgram/DPFive.py
+++ dynamicProgram/DPFive.py
@@ -68,5 +68,39 @@ def numExchange(line, n):
                     count = max(count, lens)
 
 
+def isMatch(s, p):
+    """
+    10. 正则表达式匹配(review)
+    1, If p.charAt(j) == s.charAt(i) :  dp[i][j] = dp[i-1][j-1];
+    2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
+    3, If p.charAt(j) == '*':
+    here are two sub conditions:
+               1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
+               2   if p.charAt(i-1) == s.charAt(i) or p.charAt(i-1) == '.':
+                              dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a
+                           or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
+                           or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
+    :type s: str
+    :type p: str
+    :rtype: bool
+    """
+    dp = [[False] * (len(p) + 1) for i in range(len(s) + 1)]
+    dp[0][0] = True
+    for i in range(len(p)):
+        if p[i] == "*" and dp[0][i - 1]:
+            dp[0][i + 1] = True
+
+    for i in range(len(s)):
+        for j in range(len(p)):
+            if p[j] == "." or s[i] == p[j]:
+                dp[i + 1][j + 1] = dp[i][j]
+            if p[j] == "*":
+                if p[j - 1] != s[i] and p[j - 1] != ".":
+                    dp[i + 1][j + 1] = dp[i + 1][j - 1]
+                else:
+                    dp[i + 1][j + 1] = dp[i + 1][j] or dp[i][j + 1] or dp[i + 1][j - 1]
+    return dp[-1][-1]
+
+
 if __name__ == '__main__':
-    pass
+    isMatch("qqqqqqqq", "q*")
