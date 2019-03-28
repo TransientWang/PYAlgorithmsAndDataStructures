dc937d0e33a6c8104d958ff66026f22f77aad59e
wfy815775 <815775754@qq.com>
2019-03-28 10:17:40 +0800

    217.存在重复（review）

--- stringAndArrayQuestion/ArrayTwo.py
+++ stringAndArrayQuestion/ArrayTwo.py
@@ -34,8 +34,7 @@ def gameOfLife(board):
 
 def containsDuplicate(nums):
     """
-     review
-     217.存在重复
+     217.存在重复（review）
     给定一个整数数组，判断是否存在重复元素。
     如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
     思路：除了hash空间换时间，就是先排序再找了
